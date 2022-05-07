572413b5ef6873438b7e85f2df7bb45823420752
H. Istvan <66251121+steinily@users.noreply.github.com>
2022-05-07 05:02:59 +0200

    HiperSkill Case Converter
    HiperSkill
    Project
    Case Converter
    Finished Project

---
+++ Upper or lower/main.js
@@ -0,0 +1,39 @@
+function uppercase(){
+    let text=document.getElementById('inputtext').value;
+    text=text.toUpperCase();
+    document.getElementById('inputtext').value = text;
+
+}
+
+
+function lowercase(){
+    let text=document.getElementById('inputtext').value;
+    text=text.toLowerCase();
+    document.getElementById('inputtext').value = text;
+}
+
+function propercase(){
+    let text=document.getElementById('inputtext').value;
+    let newText = text.replace(/(^|\s)\S/g, function(t) { return t.toUpperCase() });
+    document.getElementById('inputtext').value = newText;
+}
+
+function sentencecase(){
+    let text=document.getElementById('inputtext').value;
+    text=text.toLowerCase();
+    let n=text.split(".");
+    let vfinal="";
+    for(i=0;i<n.length;i++) {
+        var spaceput = ""
+        var spaceCount = n[i].replace(/^(\s*).*$/, "$1").length;
+        n[i] = n[i].replace(/^\s+/, "");
+        var newstring = n[i].charAt(n[i]).toUpperCase() + n[i].slice(1);
+        for (j = 0; j < spaceCount; j++)
+            spaceput = spaceput + " ";
+        vfinal = vfinal + spaceput + newstring + ".";
+    }
+        vfinal=vfinal.substring(0, vfinal.length - 1);
+    document.getElementById('inputtext').value = vfinal;
+
+}
+
